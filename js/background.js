0f7933d093b0aa053cfa2a8f7d8a9c568a7ebab9
CoghHs <mrpanda2412@gmail.com>
2023-04-29 22:10:49 +0900

    background img up

--- js/background.js
+++ js/background.js
@@ -1,7 +1,10 @@
 const images = [
     "0.png",
     "1.png",
-    "2.png"
+    "2.png",
+    "3.png",
+    "4.png",
+    "5.png"
 ];
 
 const chosenImages = images[Math.floor(Math.random() * images.length)];
